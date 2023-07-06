8a3159315464460366516dd2ae3b485a0de35868
Laura <ximesaenz@gmail.com>
2023-07-05 22:13:00 -0700

    version 1.2 laura add anotation  @NotEmpty to name, password, email and dateofbirth but is only workking on dateofbirth a message error will apear if dateOfBirth is empty and if the user enter a string in phone field

--- src/main/java/com/finalproject/traveltogether/controllers/UserController.java
+++ src/main/java/com/finalproject/traveltogether/controllers/UserController.java
@@ -3,11 +3,15 @@ package com.finalproject.traveltogether.controllers;
 import com.finalproject.traveltogether.pojo.User;
 import com.finalproject.traveltogether.services.UserService;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang3.math.NumberUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.validation.Errors;
+
+import javax.validation.Valid;
 
 @Controller
 @Slf4j
@@ -29,7 +33,16 @@ public class UserController {
     }
 
     @PostMapping("/save")
-    public String save(User user){
+    public String save(@Valid User user, Errors errors){
+        if (errors.hasErrors()){
+            return "profile";
+        }
+
+        if (!isValidPhoneNumber(user.getPhone())) {
+            errors.rejectValue("phone", "InvalidPhone", "Por favor, ingresa un número válido");
+            return "profile";
+        }
+
         userService.save(user);
         return "redirect:/";
     }
@@ -51,5 +64,15 @@ public class UserController {
     public String delete2(User user){
         userService.delete(user);
         return "redirect:/";
+    }
+
+    private boolean isValidPhoneNumber(String phone) {
+        // Realiza la validación adicional antes de la validación con expresiones regulares
+        if (!NumberUtils.isCreatable(phone)) {
+            return false;
+        }
+
+        // Verifica si el número contiene solo dígitos utilizando una expresión regular
+        return phone.matches("\\d+");
     }
 }
